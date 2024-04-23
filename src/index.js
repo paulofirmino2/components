ab0df34b833da4faeab7888029a9e26dc09c1cb4
PAULO OTAVIO FIRMINO SILVA <P0659243@portoseguro.com.br>
2024-04-23 11:09:57 -0300

    teste

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-console.log('9cdd8093aec7079');
-console.log('edf067d20cd10');
-console.log('ce87bf4f9aee');
+console.log("9cdd8093aec7079");
+console.log("edf067d20cd10");
+console.log("ce87bf4f9aee");
+console.log("ss");
